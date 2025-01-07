478a46adbb1b171ef0c611c8b4d1f44cdf581e8f
primnik3368 <vunderddd@gmail.com>
2025-01-07 22:46:44 +0300

    пмропр

--- install.sh
+++ install.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 echo start install
 pkg install python3
-pip3 install -r -q requirements.txt
+pip install -r -q requirements.txt
 mv bot.sh ..
 cd ..
-chmod +x bot.sh
 echo install is done.
 echo exit...
